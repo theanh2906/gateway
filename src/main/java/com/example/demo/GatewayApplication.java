3aefbbb468ff2b92fd9ee50e7509f392d9649bdd
Anh Tang <anh.tang@opswat.com>
2024-01-26 17:44:18 +0700

    update

--- src/main/java/com/example/demo/GatewayApplication.java
+++ src/main/java/com/example/demo/GatewayApplication.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = {"Content-Type", "Authorization"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
